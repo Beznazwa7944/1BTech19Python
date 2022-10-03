00849879c593e118bd4f4d3d7c914ca0574c320b
1b19MP <114583801+Beznazwa7944@users.noreply.github.com>
2022-10-03 10:19:23 +0000

    Zrobiono zadanie 1 i 2

---
+++ Kartapracy1.py
@@ -0,0 +1,9 @@
+#Zad 1
+# a = int(input("Wprowadź liczbę 1: ", ))
+# b = int(input("Wprowadź liczbę 2: ", ))
+# c = (a**2 + b**2)
+# print("Pole to:", c, " cm^2")
+#Zad 2
+a = int(input())
+b = int(input())
+print((a+b)*2)
