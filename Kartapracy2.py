8d9df3d37d556093cb0d34072fa744af7b6564f8
1b19MP <114583801+Beznazwa7944@users.noreply.github.com>
2022-10-10 10:20:00 +0000

    Rozpoczęcie

---
+++ Kartapracy2.py
@@ -0,0 +1,34 @@
+#Zad 1
+# n = int(input())
+
+# if n % 3 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#Zad 2
+# a = int(input())
+# if a>=100 and a<1000 and  a % 17 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#Zad3
+# a = int(input("Wpisz wiek: " ))
+# if a >= 18:
+#   print("Dostałeś zezwolenie na na zobaczenie tej wiadomości użytkowniku!")
+# else:
+#   print("Złamałeś zasady licencji!")
+
+#Zad4
+
+# Waga = int(input("Podaj wagę ciężarówki (Tony): " ))
+# if Waga <= 20:
+#   print("Dostęp zezwolony")
+# else:
+#   print("Nie możesz wjechać")
+
+#Zad 5
+a = int(input("podaj 1 liczbę: " ))
+b = int(input("podaj 2 liczbę: " ))
+c = int(input("podaj 3 liczbę: " ))
