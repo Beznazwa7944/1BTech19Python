0b11a51a7d08a52c8df2608561a30c62ac0d67bf
1b19MP <114583801+Beznazwa7944@users.noreply.github.com>
2022-10-16 17:31:51 +0000

    Debugowanie

--- Kartapracy2a.py
+++ Kartapracy2a.py
@@ -8,17 +8,17 @@ else:
 
 #Zad2
 import math
-a = int(input())
-g = int(input())
+a = int(input("Wpisz 1 Liczbę: "))
+g = int(input("Wpisz 2 Liczbę: "))
 if (a+g)/2 > math.sqrt(a*g):
   print("TAK")
 else:
   print("NIE")
 
 #Zad3
-k = int(input())
-m = int(input())
-l = int(input())
+k = int(input("Wpisz 1 Liczbę: "))
+m = int(input("Wpisz 2 Liczbę: "))
+l = int(input("Wpisz 3 Liczbę: "))
 if k==l:
   print("TAK")
   print(k, l)
@@ -32,25 +32,25 @@ else:
   print("NIE")
 
 #Zad4
-a = int(input("1: "))
-b = int(input("2: "))
-c = int(input("3: "))
-d = int(input("4: "))
+a = int(input("Wpisz 1 Liczbę: "))
+b = int(input("Wpisz 2 Liczbę: "))
+c = int(input("Wpisz 3 Liczbę: "))
+d = int(input("Wpisz 4 Liczbę: "))
 print(min(a,b,c,d))
 
 #Zad5
-a = int(input())
-b = int(input())
-c = int(input())
+a = int(input("Bok 1: "))
+b = int(input("Bok 2: "))
+c = int(input("Bok 3: "))
 if a<(b+c) and a>(b-c) and b<(a+c) and b>(a-c) and c<(a+b) and c>(a-b):
   print("TAK")
 else:
   print("NIE")
 
 #Zad6
-a = int(input())
-b = int(input())
-c = int(input())
+a = int(input("Bok 1: "))
+b = int(input("Bok 2: "))
+c = int(input("Bok 3: "))
 
 if c < (a+b) and c**2 < (a**2+b**2):
   print("Ostrokątny")
