a63a8fe9796ee5095b0884289e5504076c3d109e
1b19MP <114583801+Beznazwa7944@users.noreply.github.com>
2022-11-01 15:34:24 +0000

    Początek pracy

---
+++ Kartapracy3b.py
@@ -0,0 +1,33 @@
+#Zad 1
+# for i in range(1, 31, 1):
+#   print(i, "Dzień listopada")
+
+#Zad 2
+# for i in range(1, 200, 2):
+#   i = i**2
+#   print(i)
+
+#Zad 3
+# for i in range (1000, 10000, 1):
+#   if i%379 == 0:
+#     print(i)
+
+#Zad 4
+# for i in range (100, 1000, 1):
+#   if i%5 == 0 and i%6 == 0:
+#     print(i, "Nie podzielne przez 11")
+#   if i%5 == 0 and i%6 == 0 and i%11 == 0:
+#     print(i, "Podzielne przez 11")
+
+#Zad 5
+# nliczb = int(input("Ile liczb chcesz wprowadzić? " ))
+# a = 0
+# for i in range (1, nliczb + 1):
+#   n = int(input("Wprowadź liczbę: "))
+#   a = a + n
+# print("Suma liczb to ", a)
+
+#Zad 6
+k = int(input("Podaj liczbę k: "))
+for i in range(2, k):
+  if 
